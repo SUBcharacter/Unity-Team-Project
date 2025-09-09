8ed2daac018d403ad3b448594f1e32ed75e383c2
SUBcharacter <farg557@gmail.com>
2025-09-10 02:08:13 +0900

    플레이어 양옆이동 완료
    등속 이동 확인

--- Project/Assets/Script/Player.cs
+++ Project/Assets/Script/Player.cs
@@ -5,6 +5,12 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rigid;
     
+    public Vector2 inputVec;
+    public float speed;
+    
+    public bool isGround = false;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Awake()
@@ -25,21 +31,35 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+        rigid.linearVelocityX = inputVec.x * speed;
+        if (Mathf.Abs(inputVec.x) < 1)
+        {
+            rigid.linearVelocityX -= rigid.linearVelocityX;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!collision.gameObject.CompareTag("Obstacle"))
-             return;
+        if(collision.gameObject.CompareTag("Terrain"))
+        {
+            isGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
 
-        transform.gameObject.SetActive(false);
+        }
     }
 
     void Control()
     {
-
+        
     }
+
+    public void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+    
 
     void Retry()
     {
