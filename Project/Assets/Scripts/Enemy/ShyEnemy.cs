828890853795d75440f29acb9e2fcf20f6a215c6
gydud0819 <pstar0819@naver.com>
2025-09-14 00:17:28 +0900

    (2025-09-13) 적 ai 테스트

---
+++ Project/Assets/Scripts/Enemy/ShyEnemy.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+
+public class ShyEnemy : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] private float moveSpeed = 3.0f;
+    [SerializeField] private float stopDistance = 0.1f;
+
+    Rigidbody2D playerRb;
+
+    private Vector2 lastPlayerPos;        //  플레이어가 안움직일 때
+
+    private float playerLookDir ;      // 플레이어가 바라보는 방향
+    private float toEnemyDir;      // 플레이어와 적의 방향
+    private bool isSameDir;       // 같은 방향을 보고 있는지
+    private void Awake()
+    {
+        playerRb = player.GetComponent<Rigidbody2D>();
+       
+    }
+
+    private void Update()
+    {
+        playerLookDir = Mathf.Sign(player.localScale.x);
+        toEnemyDir = Mathf.Sign(transform.position.x - player.position.x);
+        isSameDir = playerLookDir != toEnemyDir;
+        //float PlayerMoveDis = Vector3.Distance(transform.position, player.position);
+        //Vector3 targetPos = player.position;
+        //if(PlayerMoveDis > stopDistance)
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        //}
+
+        //lastPlayerPos = player.position;
+
+
+
+        // 플레이어가 움직이면 따라오기
+
+        /// <summary>
+        /// 유니티 6에서부터 Velocity가 아닌 linearVelocity로 사용한다
+        /// </summary>
+
+        if (isSameDir)      // magnitude : 벡터의 크기(길이) 
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+        // 멈추면 가만히
+
+
+    }
+}
--- Project/Assets/Scripts/Player/PlayerMove.cs
+++ Project/Assets/Scripts/Player/PlayerMove.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody2D rigidbody2D;
+    PlayerAction playerAction;
+
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+
+
+    [SerializeField] private float moveSpeed = 5.0f;
+    private void Awake()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        playerAction = new PlayerAction();
+        playerAction.Player.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // 방향키로 플레이어 움직이는 코드
+        Vector2 moveDir = playerAction.Player.Move.ReadValue<Vector2>();
+
+        if (moveDir.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            spriteRenderer.sprite = frontSprite;
+        }
+        else if (moveDir.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            spriteRenderer.sprite = frontSprite;
+        }
+        else
+        {
+            // 이동 멈췄을 때 → 뒤돌았다고 가정 (원하면 방향 유지해도 됨)
+            spriteRenderer.sprite = backSprite;
+        }
+
+        rigidbody2D.linearVelocity = moveDir * moveSpeed;
+
+
     }
 }
