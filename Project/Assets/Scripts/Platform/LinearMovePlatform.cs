a762841d0ce79b98e82c9138781ec08d7e2ca3e3
cjiho0312 <cjiho0312@naver.com>
2025-09-26 18:35:17 +0900

    선형 이동 플랫폼 수정

--- Project/Assets/Scripts/Platform/LinearMovePlatform.cs
+++ Project/Assets/Scripts/Platform/LinearMovePlatform.cs
@@ -4,28 +4,57 @@ public class LinearMovePlatform : MonoBehaviour
 {
     // 선형 이동
 
-    Vector3 station1;
-    [SerializeField] Vector2 station2;
+    [SerializeField] Vector3 station2;
+    [SerializeField] float speed = 2f;
 
-    Vector2 direction;
-
+    private Vector3 station1;
+    private Vector2 direction;
+    private bool toStation2;
 
     private void Start()
     {
         station1 = transform.position;
-
+        toStation2 = true;
         Caldir();
-    }
-
-    private void FixedUpdate()
-    {
-
     }
 
     void Caldir()
     {
-        direction = new Vector2(Mathf.Abs(station1.x/station2.x), Mathf.Abs(station1.y/station2.y));
+        Vector3 destination;
+
+        if (toStation2) { destination = station2; }
+        else { destination = station1; }
+
+        direction = new Vector2(destination.x - transform.position.x, destination.y - transform.position.y).normalized;
     }
+
+    private void MovePlatform()
+    {
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+
+        float distance;
+
+        if (toStation2)
+        {
+            distance = Vector3.Distance(transform.position, station2);
+        }
+        else
+        {
+            distance = Vector3.Distance(transform.position, station1);
+        }
+
+        if (distance <= 0.05f) // 거의 도달했으면 방향 반전. 더 줄이면 인식 잘 안 됨.
+        {
+            toStation2 = !toStation2;
+            Caldir();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+         MovePlatform();
+    }
+
 
     
 }
